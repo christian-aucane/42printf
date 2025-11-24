2a440b4f9fddfb9bd5b2e270ebd66d302694e389
Christian Aucane <caucane@c1r15s2.marseille.42.school>
2025-11-24 15:38:06 +0100

    ...

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: caucane <christian.aucane@learner.42.tech  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 17:10:24 by caucane           #+#    #+#             */
-/*   Updated: 2025/11/17 17:10:29 by caucane          ###   ########.fr       */
+/*   Updated: 2025/11/24 14:37:23 by caucane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_PRINTF_H
 
 # include "handlers.h"
+# include <stdarg.h>
 
 int	ft_printf(const char *format, ...);
 
